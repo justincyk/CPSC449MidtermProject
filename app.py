dee4ad68b94584fd82e228c7ae93e378873bb17f
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-04-08 15:27:24 -0700

    completed authentication, creation of public and private endpoints, and creation of sitemap public enpoint that list all public enpoints

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify, abort
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, abort, make_response
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 import pymysql
 from datetime import timedelta
 from flask_cors import CORS
 import re
+from functools import wraps
+import jwt
 
 
 app = Flask(__name__, template_folder='templates')
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+SECRET = "sufferin' succotash!"
 
 app.secret_key = 'happykey'
 app.permanent_session_lifetime = timedelta(minutes=10)
@@ -18,82 +21,97 @@ app.permanent_session_lifetime = timedelta(minutes=10)
 # To connect MySQL database
 conn = pymysql.connect(
         host='localhost',
-        user='user', # set user to the username of your account - prob "root"
-        password = "test", # Change password to password you set for your database
+        user='s1akr', # set user to the username of your account - prob "root"
+        password = "hellomahbebe", # Change password to password you set for your database
         db='449_midterm',
 		cursorclass=pymysql.cursors.DictCursor
         )
 cur = conn.cursor()
 
-# Login page
-@app.route('/')
-@app.route('/login', methods =['GET', 'POST'])
+# login_required decorator definition
+def login_required(func):
+	@wraps(func)
+	def decorated_func(*args, **kwargs):
+		encoded_jwt = request.cookies.get('token')
+		if encoded_jwt:
+			user = jwt.decode(encoded_jwt, SECRET, algorithms=["HS256"])
+			if user['username'] == 'admin': return func(*args, **kwargs)
+		abort(401, description="Authorized access only! Please login.")
+	return decorated_func
+
+# login endpoint
+@app.route('/login', methods = ['POST'])
 def login():
-	msg = ''
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+	data = request.form
+	username = data.get('username')
+	password = data.get('password')
 
-		session.permanent = True
-		username = request.form['username']
-		password = request.form['password']
-		cur.execute('SELECT * FROM Accounts WHERE Username = % s AND Password = % s', (username, password, ))
-		conn.commit()
-		account = cur.fetchone()
-		if account:
+	# if required information is not present
+	if not data or not username or not password:
+		abort(401, description="username and password is required to login")
 
-			session['loggedin'] = True
-			session['id'] = account['id']
-			session['Username'] = account['Username']
-			msg = 'Logged in successfully !'
-			# direct them to the upload a file page if they successfully log in
-			return render_template('upload.html', msg = msg)
-		else:
-			msg = 'Incorrect username / password !'
+	# use information to retreive account
+	cur.execute('select * from Accounts where username = % s and password = % s', (username, password))
+	conn.commit()
+	account = cur.fetchone()
+
+	# if one exists and password matches
+	if account and account['Password'] == password:
+		# encode jwt
+		user = {
+			'username': account['Username'],
+	  	'first name': account['FirstName'],
+			'last name': account['LastName']
+		}
+		encoded_jwt = jwt.encode(user, SECRET, algorithm='HS256')
+		# set as cookie
+		resp = make_response({'message': 'login successful!!'}, 200)
+		resp.set_cookie('token', encoded_jwt)
+		return resp
 	else:
-		# if "loggedin" in session:
-		# 	# redirect them to the upload a file page if they are already logged in
-		# 	return redirect(url_for("upload_file"))
-		return render_template('login.html', msg = msg)
-	
+		# abort(401, description="incorrect username and/or password")
+		# swap commenting with above code to get postman to work properly after a successful login:
+		resp = make_response({'message': 'incorrect username and/or password'}, 401)
+		resp.set_cookie('token', '', expires=0)
+		return resp
 
-@app.route('/logout')
-def logout():
-    session.pop('loggedin', None)
-    session.pop('id', None)
-    session.pop('username', None)
-    return redirect(url_for('login'))
 
-@app.route('/register', methods =['GET', 'POST'])
-def register():
-	msg = ''
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'firstName' in request.form and 'lastName' in request.form:
-		print('reached')
-		username = request.form['username']
-		password = request.form['password']
-		retypePassword = request.form['retypePassword']
-		email = request.form['email']
-		firstName = request.form['firstName']
-		lastName = request.form['lastName']
-		cur.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
-		account = cur.fetchone()
-		print(account)
-		conn.commit()
-		if account:
-			msg = 'Account already exists!'
-		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-			msg = 'Invalid email address!'
-		elif not re.match(r'[A-Za-z0-9]+', username):
-			msg = 'name must contain only characters and numbers!'
-		elif not re.match(retypePassword, password):
-			msg = 'passwords do not match!'
-		else:
-			cur.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s, % s, % s)', (username, password, email, firstName, lastName))
-			conn.commit()
+# protected endpoint
+@app.route('/protected')
+@login_required
+def protected():
+	# protected endpoint only authenticated users can access
+	return {'message': 'You got in!  You must be special!! or just logged in...'}
 
-			msg = 'You have successfully registered!'
-	elif request.method == 'POST':
-		msg = 'Please fill out the form!'
-	return render_template('register.html', msg = msg)
+# public enpoints
+@app.route('/public')
+def public():
+	return {
+		'message': 'You got in! Pfft...so what!! So did everyone else...',
+		'view': 'public information'
+	}
 
+@app.route('/unprotected')
+def unprotected():
+	return {
+		'message': 'You got in! Pfft...so what!! So did everyone else...',
+		'view': 'public information'
+	}
+
+@app.route('/freebies')
+def freebies():
+	return {
+		'message': "You got in! Pfft...so what!! So did everyone else...",
+		'view': 'public information'
+	}
+
+# endpoint to return a list of items that can be viewed publicly
+@app.route('/sitemap')
+def sitemap():
+	return {
+		'public endpoints': ['/public', '/unprotected', '/freebies'],
+		'view': 'public information'
+	}
 
 @app.route('/upload')
 def upload_file():
@@ -102,13 +120,13 @@ def upload_file():
       return render_template("upload.html")
    # If not logged in, direct them to the login page
    return redirect(url_for('login'))
-	
+
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload():
    if request.method == 'POST':
       f = request.files['file']
-      f.save(secure_filename(f.filename)) 
+      f.save(secure_filename(f.filename))
       return 'file uploaded successfully'
-		
+
 if __name__ == '__main__':
-   app.run(debug = False)
+   app.run(host='localhost', port = int('8000'), debug = False)
